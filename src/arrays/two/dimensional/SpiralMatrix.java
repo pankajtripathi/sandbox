959dfc0bb4f32812f85000119c79a0b1f82d9892
pankajtripathi <tripathi.p@husky.neu.edu>
2018-11-07 17:48:16 -0800

    fixed issues in SpiralMatrix

--- src/arrays/two/dimensional/SpiralMatrix.java
+++ src/arrays/two/dimensional/SpiralMatrix.java
@@ -1,17 +1,21 @@
 package arrays.two.dimensional;
 
-public class SpiralMatrix {
+import java.util.ArrayList;
+import java.util.List;
+
+public class    SpiralMatrix {
     public static void main(String[] args) {
         int matrix[][] = {
                 {1, 2, 3},
                 {4, 5, 6},
                 {7, 8, 9}
         };
-        new SpiralMatrix().printSpiral(matrix);
+        System.out.println(new SpiralMatrix().printSpiral(matrix));
     }
 
-    private void printSpiral(int[][] matrix) {
-        if (matrix.length == 0 || matrix == null || matrix[0].length == 0) return;
+    private List<Integer> printSpiral(int[][] matrix) {
+        List<Integer> result = new ArrayList<>();
+        if (matrix.length == 0 || matrix == null || matrix[0].length == 0) return result;
 
         int leftCol = 0;
         int rightCol = matrix[0].length - 1;
@@ -19,30 +23,36 @@ public class SpiralMatrix {
         int topRow = 0;
         int bottomRow = matrix.length - 1;
 
-        while (leftCol <= rightCol && topRow <= bottomRow) {
+        while (result.size() < matrix.length * matrix[0].length) {
             // Print elements from left column to right column for top row
             for (int i = leftCol; i <= rightCol; i++) {
-                System.out.println(matrix[topRow][i]);
+                result.add(matrix[topRow][i]);
             }
             topRow++;
 
             // Print elements from top row to bottom row for right column
             for (int i = topRow; i <= bottomRow; i++) {
-                System.out.println(matrix[i][rightCol]);
+                result.add(matrix[i][rightCol]);
             }
             rightCol--;
 
+            // Avoid duplicate rows
+            if (bottomRow < topRow) break;
             // Print elements from right column to left column for bottom row
             for (int i = rightCol; i >= leftCol ; i--) {
-                System.out.println(matrix[bottomRow][i]);
+                result.add(matrix[bottomRow][i]);
             }
             bottomRow--;
 
+            // Avoid duplicate columns
+            if (rightCol < leftCol) break;
             // Print elements from bottom row to top row for left column
             for (int i = bottomRow; i >= topRow ; i--) {
-                System.out.println(matrix[i][leftCol]);
+                result.add(matrix[i][leftCol]);
             }
             leftCol++;
         }
+
+        return result;
     }
 }
