5108956a62569c7d4e21be4fe80ecceb4712ed29
pankajtripathi <tripathi.p@husky.neu.edu>
2018-11-19 19:39:03 -0800

    removed Coordinate class from SearchMaze

--- src/graphs/Coordinate.java
+++ src/graphs/Coordinate.java
@@ -1,4 +1,22 @@
 package graphs;
 
-public class Coordinate {
+class Coordinate {
+    int x;
+    int y;
+
+    Coordinate(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+
+        if (obj == null || getClass() != obj.getClass()) return false;
+
+        Coordinate that = (Coordinate) obj;
+        return x == that.x && y == that.y;
+    }
 }
