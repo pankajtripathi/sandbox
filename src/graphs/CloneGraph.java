07b649d8e6add1dc3a4eb3e5457ba1fd22cc2241
pankajtripathi <tripathi.p@husky.neu.edu>
2018-12-17 18:29:29 -0800

    updated CloneGraph

--- src/graphs/CloneGraph.java
+++ src/graphs/CloneGraph.java
@@ -3,58 +3,61 @@ package graphs;
 import java.util.*;
 
 /* Consider a vertex type for a directed graph in which there are two fields: an integer label and  a list of
-   references to other vertices. Design an algorithm that takes a reference to a vertex u and creates a copy of the
-   graph on the vertices reachable from u. Return a copy of u
+   references to other neighbors. Design an algorithm that takes a reference to a vertex u and creates a copy of the
+   graph on the neighbors reachable from u. Return a copy of u
 */
 public class CloneGraph {
     static class GraphVertex {
         int label;
-        List<GraphVertex> vertices;
+        List<GraphVertex> neighbors;
 
         GraphVertex(int label) {
             this.label = label;
-            vertices = new ArrayList<>();
+            neighbors = new ArrayList<>();
         }
     }
 
-    public GraphVertex cloneGraph(GraphVertex u) {
-        if (u == null) return null;
+    public GraphVertex cloneGraph(GraphVertex node) {
+        if (node == null) return null;
 
         Map<GraphVertex, GraphVertex> vertexMap = new HashMap<>();
         Queue<GraphVertex> queue = new ArrayDeque<>();
 
-        vertexMap.put(u, new GraphVertex(u.label));
-        queue.add(u);
+        GraphVertex head = new GraphVertex(node.label);
+        vertexMap.put(node, head);
+        queue.add(node);
 
         while (!queue.isEmpty()) {
-            GraphVertex g = queue.poll();
+            GraphVertex current = queue.poll();
 
-            for (GraphVertex e : g.vertices) {
-                // copy vertex e
-                if (!vertexMap.containsKey(e)) {
-                    vertexMap.put(e, new GraphVertex(e.label));
-                    queue.add(e);
+            for (GraphVertex neighbor : current.neighbors) {
+                // copy vertex neighbor
+                if (!vertexMap.containsKey(neighbor)) {
+                    GraphVertex copy = new GraphVertex(neighbor.label);
+                    vertexMap.put(neighbor, copy);
+                    queue.add(neighbor);
+                    vertexMap.get(current).neighbors.add(copy);
                 }
 
                 // copy edge
-                vertexMap.get(g).vertices.add(vertexMap.get(e));
+                vertexMap.get(current).neighbors.add(vertexMap.get(neighbor));
             }
         }
 
-        return vertexMap.get(u);
+        return head;
     }
 
     public static void main(String[] args) {
         GraphVertex zero = new GraphVertex(0);
-        zero.vertices.add(new GraphVertex(1));
-        zero.vertices.add(new GraphVertex(2));
+        zero.neighbors.add(new GraphVertex(1));
+        zero.neighbors.add(new GraphVertex(2));
 
         GraphVertex two = new GraphVertex(2);
-        two.vertices.add(new GraphVertex(0));
-        two.vertices.add(new GraphVertex(3));
+        two.neighbors.add(new GraphVertex(0));
+        two.neighbors.add(new GraphVertex(3));
 
         GraphVertex returnedVertex = new CloneGraph().cloneGraph(zero);
-        for (GraphVertex v : returnedVertex.vertices) {
+        for (GraphVertex v : returnedVertex.neighbors) {
             System.out.println(v.label);
         }
     }
