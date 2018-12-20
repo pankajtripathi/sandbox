0150a9a81133ae579bcb7dc7d9a222464b759843
pankajtripathi <tripathi.p@husky.neu.edu>
2018-12-19 18:00:57 -0800

    added question

--- src/graphs/GraphValidTree.java
+++ src/graphs/GraphValidTree.java
@@ -1,5 +1,14 @@
 package graphs;
 
+/*
+Given n nodes labeled from 0 to n-1 and a list of undirected edges (each edge is a pair of nodes),
+write a function to check whether these edges make up a valid tree.
+
+Example 1:
+Input: n = 5, and edges = [[0,1], [0,2], [0,3], [1,4]]
+
+Output: true
+* */
 public class GraphValidTree {
     public static void main(String[] args) {
         int[][] edges1 = {
