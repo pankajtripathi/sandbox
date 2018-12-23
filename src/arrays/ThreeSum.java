f113b9fa2cf7a343faf762a8c272369591cdedbf
pankajtripathi <tripathi.p@husky.neu.edu>
2018-12-22 18:23:44 -0800

    added ThreeSum

--- src/arrays/ThreeSum.java
+++ src/arrays/ThreeSum.java
@@ -1,4 +1,45 @@
 package arrays;
 
+import java.util.*;
+
 public class ThreeSum {
+    public static void main(String[] args) {
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+        System.out.println(new ThreeSum().threeSum(nums));
+    }
+
+    private List<List<Integer>> threeSum(int[] nums) {
+        Set<List<Integer>> result = new HashSet<>();
+        if (nums.length < 3) return new ArrayList<>(result);
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            int complement = -nums[i];
+            int low = i + 1;
+            int high = nums.length - 1;
+
+            while (low < high) {
+                if (nums[low] + nums[high] == complement) {
+                    result.add(Arrays.asList(nums[i], nums[low], nums[high]));
+                    low++;
+                    high--;
+
+                    // skip already seen numbers
+                    while (low < high && low > 0 && nums[low - 1] == nums[low])
+                        low++;
+                    // skip already seen numbers
+                    while (low < high && high < nums.length - 1 && nums[high] == nums[high + 1])
+                        high--;
+
+                }
+                else if (nums[low] + nums[high] < complement) {
+                    low++;
+                } else if (nums[low] + nums[high] > complement) {
+                    high--;
+                }
+            }
+        }
+
+        return new ArrayList<>(result);
+    }
 }
