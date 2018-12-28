4f6c431c37b241f3f1547616aa769a28cf5b819f
pankajtripathi <tripathi.p@husky.neu.edu>
2018-12-27 23:41:47 -0500

    added multiple approaches for FlattenBTToLL

--- src/trees/FlattenBTToLL.java
+++ src/trees/FlattenBTToLL.java
@@ -1,5 +1,7 @@
 package trees;
 
+import java.util.Stack;
+
 public class FlattenBTToLL {
     Node root = null;
 
@@ -10,32 +12,69 @@ public class FlattenBTToLL {
         tree.root.right = new Node(3);
         tree.root.right.left = new Node(4);
 
-        new FlattenBTToLL().flatten(tree.root);
+//        new FlattenBTToLL().flatten(tree.root);
+//        System.out.println(new BinaryTreeBFSWithQueue().bfs(tree.root));
+
+//        new FlattenBTToLL().flattenRec(tree.root);
+//        System.out.println(new BinaryTreeBFSWithQueue().bfs(tree.root));
+
+        new FlattenBTToLL().flattenToLL(tree.root);
         System.out.println(new BinaryTreeBFSWithQueue().bfs(tree.root));
     }
 
+    // Go down through the left, when right is not null, push right to stack.
     private void flatten(Node root) {
         if (root == null) return;
-        Node node = root;
+        Stack<Node> stack = new Stack<>();
+        Node p = root;
 
-        while (node.left != null || node.right != null) {
-            if (node.left != null) {
-                Node rightChild = node.right;
-
-                // node node right becomes node left and left is null
-                node.right = node.left;
-                node.left = null;
-
-                // Remaining left nodes .. here rightRem is node.right coz node.right has left node now
-                Node rightRem = node.right;
-                while (rightRem.right != null) {
-                    rightRem = rightRem.right;
-                }
-
-                rightRem.right = rightChild;
+        while (p != null || !stack.empty()) {
+            if (p.right != null) {
+                stack.push(p.right);
             }
 
-            node = node.right;
+            if (p.left != null) {
+                p.right = p.left;
+                p.left = null;
+            } else if (!stack.empty()) {
+                p.right = stack.pop();
+            }
+
+            p = p.right;
+        }
+    }
+
+    Node pre = null;
+    private void flattenRec(Node root) {
+        helper(root);
+    }
+
+    private void helper(Node root) {
+        if (root == null) return;
+        helper(root.right); // right in rec stack
+        helper(root.left); // left in rec stack
+        root.left = null; // set left as null
+        root.right = pre; // set right to pre
+        pre = root;  // pre to root
+    }
+
+    private void flattenToLL(Node root) {
+        if (root == null) return;
+        Node p = root;
+
+        while (p.left != null || p.right != null) {
+            if (p.left != null) {
+                Node rightChild = p.right;
+                p.right = p.left;
+                p.left = null;
+
+                Node rm = p.right;
+                while (rm.right != null) {
+                    rm = rm.right;
+                }
+                rm.right = rightChild;
+            }
+            p = p.right;
         }
     }
 }
