50ce15d11826bb660d97603b0e265ec956e4db1f
pankajtripathi <tripathi.p@husky.neu.edu>
2018-12-28 17:04:01 -0500

    refacrtored HouseRobber2

--- src/dp/HouseRobber2.java
+++ src/dp/HouseRobber2.java
@@ -7,7 +7,7 @@ package dp;
 * */
 public class HouseRobber2 {
     public static void main(String[] args) {
-        int[] nums = {1, 2, 3, 1};
+        int[] nums = {2, 3, 2};
         System.out.println(new HouseRobber2().rob(nums));
     }
 
@@ -17,27 +17,27 @@ public class HouseRobber2 {
         if (nums.length == 2) return Math.max(nums[0], nums[1]);
 
         // first is included and last is excluded
-        int max1 = robHelper(nums, 2, nums.length - 2);
+        int max1 = robHelper(nums, 0, nums.length - 2);
 
         // first is excluded and last is included
         int max2 = robHelper(nums, 1, nums.length - 1);
 
         // added first element to max1 coz we did not add it while calculating max1
-        return Math.max(nums[0] + max1, max2);
+        return Math.max(max1, max2);
     }
 
     private int robHelper(int[] nums, int from, int to) {
         if (from > to) return 0;
         if (from == to) return nums[from];
 
-        int[] dp = new int[to - from + 1];
-        dp[0] = nums[from];
-        dp[1] = Math.max(dp[0], nums[from + 1]);
+        int[] dp = new int[nums.length];
+        dp[from] = nums[from];
+        dp[from + 1] = Math.max(dp[from], nums[from + 1]);
 
-        for (int i = 2; i < to - from; i++) {
+        for (int i = from + 2; i <= to; i++) {
             dp[i] = Math.max(dp[i - 1], nums[i] + dp[i - 2]);
         }
 
-        return dp[to - from];
+        return dp[to];
     }
 }
