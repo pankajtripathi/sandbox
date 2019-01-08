b917af33547ce5b4f8d7a7927a938f8f766973b5
pankajtripathi <tripathi.p@husky.neu.edu>
2019-01-07 18:57:33 -0800

    minor refactoring in ThreeSum

--- src/arrays/ThreeSum.java
+++ src/arrays/ThreeSum.java
@@ -13,7 +13,9 @@ public class ThreeSum {
         if (nums.length < 3) return new ArrayList<>(result);
         Arrays.sort(nums);
 
-        for (int i = 0; i < nums.length - 1; i++) {
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i != 0 && nums[i - 1] == nums[i]) continue;
+
             int complement = -nums[i];
             int low = i + 1;
             int high = nums.length - 1;
