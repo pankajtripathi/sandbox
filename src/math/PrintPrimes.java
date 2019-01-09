a3052fcd872d8bae34444a22c3b6a535b6e9fb04
pankajtripathi <tripathi.p@husky.neu.edu>
2019-01-08 18:50:35 -0800

    added PrintPrimes

--- src/math/PrintPrimes.java
+++ src/math/PrintPrimes.java
@@ -1,4 +1,32 @@
 package math;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PrintPrimes {
+    public static void main(String[] args) {
+        new PrintPrimes().printPrimes(10);
+    }
+
+    private void printPrimes(int num) {
+        List<Integer> primeNumbers = new ArrayList<>();
+        primeNumbers.add(2);
+
+        for (int i = 3; i < num; i+=2) {
+            boolean isPrime = true;
+
+            for (int prime : primeNumbers) {
+                if (i % prime == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime) {
+                primeNumbers.add(i);
+            }
+        }
+
+        System.out.println(primeNumbers);
+    }
 }
