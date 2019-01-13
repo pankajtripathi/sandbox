a6692f8b535531f2b3273e8e0dbf1d723becdaf2
pankajtripathi <tripathi.p@husky.neu.edu>
2019-01-12 18:30:44 -0800

    Added SortStack

--- src/stack/SortStack.java
+++ src/stack/SortStack.java
@@ -1,4 +1,35 @@
 package stack;
 
+import java.util.Stack;
+
 public class SortStack {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(3);
+        stack.push(2);
+        stack.push(4);
+
+        System.out.println(new SortStack().sort(stack));
+    }
+
+    private Stack<Integer> sort(Stack<Integer> stack) {
+        if (stack == null || stack.size() == 0) return stack;
+        Stack<Integer> newStack = new Stack<>();
+        newStack.push(stack.pop()); // store top in new stack
+
+        while (!stack.empty()) {
+            int temp = stack.pop();
+
+            // if top of stack is greater than the current top of newStack then add top of newStack to stack
+            // and temp to new stack
+            while (!newStack.empty() && temp > newStack.peek()) {
+                stack.push(newStack.pop());
+            }
+
+            newStack.push(temp);
+        }
+
+        return newStack;
+    }
 }
