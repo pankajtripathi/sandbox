7f14887dd575f43d2a251d5623ab2d28481e96f3
pankajtripathi <tripathi.p@husky.neu.edu>
2019-01-13 17:19:27 -0800

    added InsertNodeInBST

--- src/trees/InsertNodeInBST.java
+++ src/trees/InsertNodeInBST.java
@@ -1,4 +1,33 @@
 package trees;
 
 public class InsertNodeInBST {
+    Node root = null;
+
+    public static void main(String[] args) {
+        InsertNodeInBST obj = new InsertNodeInBST();
+        obj.insertNode(3);
+        obj.insertNode(6);
+        obj.insertNode(2);
+
+        System.out.println(new BinaryTreeBFSWithQueue().bfs(obj.root));
+    }
+
+    public void insertNode(int key) {
+        root = insertNodeRec(root, key);
+    }
+
+    private Node insertNodeRec(Node root, int key) {
+        if (root == null) {
+            root = new Node(key);
+            return root;
+        }
+
+        if (key < root.data) {
+            root.left = insertNodeRec(root.left, key);
+        } else if (key > root.data) {
+            root.right = insertNodeRec(root.right, key);
+        }
+
+        return root;
+    }
 }
