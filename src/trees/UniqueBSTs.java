dbd8e84f1596d641fa2159682507fbd1623da3d8
pankajtripathi <tripathi.p@husky.neu.edu>
2019-03-11 22:23:07 -0700

    2nd approach

--- src/trees/UniqueBSTs.java
+++ src/trees/UniqueBSTs.java
@@ -10,6 +10,7 @@ We skip the proof here, which one can find following the above reference.
 public class UniqueBSTs {
     public static void main(String[] args) {
         System.out.println(new UniqueBSTs().numTrees(3));
+        System.out.println(new UniqueBSTs().numTreesDP(3));
     }
 
     private int numTrees(int n) {
@@ -19,5 +20,18 @@ public class UniqueBSTs {
         }
 
         return C;
+    }
+
+    public int numTreesDP(int n) {
+        int [] G = new int[n+1];
+        G[0] = G[1] = 1;
+
+        for(int i=2; i<=n; ++i) {
+            for(int j=1; j<=i; ++j) {
+                G[i] += G[j-1] * G[i-j];
+            }
+        }
+
+        return G[n];
     }
 }
