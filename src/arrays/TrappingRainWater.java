fb97afdc49cbeb7e825d13553ce68905095ee6aa
pankajtripathi <tripathi.p@husky.neu.edu>
2021-03-25 19:28:48 -0700

    Updating TrappingRainWater

--- src/arrays/TrappingRainWater.java
+++ src/arrays/TrappingRainWater.java
@@ -1,44 +1,39 @@
 package arrays;
 
 public class TrappingRainWater {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         int arr[] = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
-        int n = arr.length;
-        System.out.println("Maximum water that can be accumulated is: " + new TrappingRainWater().findWater(arr, n));
-
+        System.out.println("Maximum water that can be accumulated is: " + new TrappingRainWater().trap(arr));
     }
 
-    private int findWater(int[] arr, int n) {
-        int leftMax = 0;
-        int rightMax = 0;
-        int low = 0;
-        int high = n - 1;
-
-        int result = 0;
-        while (low <= high) {
-            if (arr[low] < arr[high]) {
-                if (arr[low] > leftMax)
-                    // update max in left
-                    leftMax = arr[low];
-                else
-                    // water on curr element =
-                    // max - curr
-                    result += leftMax - arr[low];
-
-                low++;
-            } else {
-                if (arr[high] > rightMax)
-                    // update max in right
-                    rightMax = arr[high];
-                else
-                    // water on curr element =
-                    // max - curr
-                    result += rightMax - arr[high];
-                high--;
+    public int trap(int[] height) {
+        if (height == null || height.length == 0) return 0;
+        int maxHeightIndex = 0;
+        int maxHeight = 0;
+        // Get index of the tower with max height
+        for (int i = 0; i < height.length; i++) {
+            if (height[i] > maxHeight) {
+                maxHeight = height[i];
+                maxHeightIndex = i;
             }
         }
 
-        return result;
+        // call helper 1. from left to maxHeightIndex 2. from right to maxHeightIndex
+        return trapHelper(height, 0, maxHeightIndex, 1) +
+                trapHelper(height, height.length - 1, maxHeightIndex, -1);
+    }
+
+    public int trapHelper(int[] height, int start, int end, int step) {
+        int sum = 0, highestLevelSeen = Integer.MIN_VALUE;
+        // Loop from start to end. If current tower is highest mark it else heighest tower minus the height of current
+        for (int i = start; i != end; i+= step) {
+            if (height[i] > highestLevelSeen) {
+                highestLevelSeen = height[i];
+            } else {
+                sum += highestLevelSeen - height[i];
+            }
+        }
+
+        return sum;
     }
 }
