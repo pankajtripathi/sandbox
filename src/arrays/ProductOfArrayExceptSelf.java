f90865f11b04469dd6a508ca492713456ce86385
pankajtripathi <tripathi.p@husky.neu.edu>
2021-03-26 17:28:38 -0700

    Updating ProductOfArrayExceptSelf

--- src/arrays/ProductOfArrayExceptSelf.java
+++ src/arrays/ProductOfArrayExceptSelf.java
@@ -12,15 +12,17 @@ public class ProductOfArrayExceptSelf {
         int[] result = new int[nums.length];
         result[0] = 1;
 
-        // multiply current result and the num to get result for next num
-        for (int i = 0; i < nums.length - 1; i++) {
-            result[i + 1] = result[i] * nums[i];
+        for (int i = 1; i < nums.length; i++) {
+            // answer[i - 1] already contains the product of elements to the left of 'i - 1'
+            // simply multiplying it with nums[i - 1] would give the product of all
+            // elements to the left of index 'i'
+            result[i] = result[i - 1] * nums[i - 1];
         }
 
         int right = 1;
-        // current result is right multiplied by result
-        // right becomes right * num
         for (int i = nums.length - 1; i >= 0; i--) {
+            // For the index 'i', R would contain the
+            // product of all elements to the right. We update R accordingly
             result[i] *= right;
             right *= nums[i];
         }
