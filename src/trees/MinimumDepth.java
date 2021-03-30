cbbd1db2fd4cdc588620bb9369642b6363527b78
pankajtripathi <tripathi.p@husky.neu.edu>
2021-03-29 18:58:07 -0700

    Updated trees-MinimumDepth

--- src/trees/MinimumDepth.java
+++ src/trees/MinimumDepth.java
@@ -17,7 +17,6 @@ public class MinimumDepth {
 
     private int minimumDepth(Node root) {
         if (root == null) return 0;
-        if (root.left == null && root.right == null) return 1;
         if (root.left == null) return 1 + minimumDepth(root.right);
         if (root.right == null) return 1 + minimumDepth(root.left);
 
