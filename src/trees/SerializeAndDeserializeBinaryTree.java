ddb0e041f7ec5711ce11e9b69588a1cd8ee0304d
pankajtripathi <tripathi.p@husky.neu.edu>
2021-04-13 13:25:27 -0700

    Added trees-SerializeAndDeserializeBinaryTree

--- src/trees/SerializeAndDeserializeBinaryTree.java
+++ src/trees/SerializeAndDeserializeBinaryTree.java
@@ -1,2 +1,58 @@
-package trees;public class SerializeAndDeserializeBinaryTree {
+package trees;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SerializeAndDeserializeBinaryTree {
+    Node root = null;
+    public static void main(String[] args) {
+        SerializeAndDeserializeBinaryTree tree = new SerializeAndDeserializeBinaryTree();
+        tree.root = new Node(1);
+        tree.root.left = new Node(2);
+        tree.root.right = new Node(3);
+        tree.root.right.left = new Node(4);
+        tree.root.right.right = new Node(5);
+
+        String treeStruct = new SerializeAndDeserializeBinaryTree().serialize(tree.root);
+        System.out.println(treeStruct);
+
+        System.out.println(new BinaryTreeBFSWithQueue().bfs(new SerializeAndDeserializeBinaryTree().deserialize(treeStruct)));
+    }
+
+    public String serialize(Node root) {
+        return preOrder(root, "");
+    }
+
+    private String preOrder(Node root, String res) {
+        if (root == null) {
+            res += "null,";
+        } else {
+            res += String.valueOf(root.data) + ",";
+            res = preOrder(root.left, res);
+            res = preOrder(root.right, res);
+        }
+        return res;
+    }
+
+    public Node deserialize(String data) {
+        String[] entries = data.split(",");
+        List<String> tree = new ArrayList<>(Arrays.asList(entries));
+        return recreate(tree);
+    }
+
+    private Node recreate(List<String> tree) {
+        if (tree.get(0).equals("null")) {
+            tree.remove(0);
+            return null;
+        }
+
+        Node root = new Node(Integer.parseInt(tree.get(0)));
+        tree.remove(0);
+
+        root.left = recreate(tree);
+        root.right = recreate(tree);
+
+        return root;
+    }
 }
